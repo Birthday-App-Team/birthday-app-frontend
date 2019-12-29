5f944ecab8cc0a4981ed96f88b1675bd13b37607
Genevieve Heald <gennaheald@outlook.com>
2019-12-29 14:25:07 +0000

    first commit

--- src/Components/AddPerson.js
+++ src/Components/AddPerson.js
@@ -1,110 +1,172 @@
-import React from "react";
-import "../App.css";
-import moment from "moment";
-import "font-awesome/css/font-awesome.min.css";
+import React from 'react';
+import '../App.css';
 
 class AddPerson extends React.Component {
+
   //initial state
   state = {
     name: "",
     dateSelected: "",
-    noteText: "",
-    showModal: false
+    note: "",
+    gender: "f",
+    showModal: false,
+    birthday: ""
   };
 
-  updateNoteText = event => {
-    this.setState({
-      newTaskText: event.target.value
-    });
-    console.log("hello");
-  };
-
+  // shows modal
   handleClick = () => {
-    console.log("hi");
-    if (this.state.showModal === false) {
-      this.setState({
-        showModal: true
-      });
-      console.log(this.state.showModal);
-    }
+    this.setState({
+      showModal: true
+    });
   };
 
+  // hides modal
   handleModalDismiss = () => {
     this.setState({
       showModal: false
     });
   };
 
-  handleDateChange = e => {
+  handleClickAdd = () => {
+    this.props.addBirthdayFunc(
+      this.state.name,
+      this.state.birthday,
+      this.state.note,
+    );
     this.setState({
-      DOB: e.target.value
+      showModal: false
+    });
+  };
+
+  updateNoteText = e => {
+    this.setState({
+      note: e.target.value
+    });
+  };
+
+  handleBirthday = e => {
+    this.setState({
+      birthday: e.target.value
+    });
+  };
+
+  handleNewName = e => {
+    this.setState({
+      name: e.target.value
     });
   };
 
   render() {
     return (
-      <div className="col-2">
-        <div
-          className={this.state.showModal ? "modal isVisible" : "modal"}
-          tabIndex="-1"
-          role="dialog"
-        >
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title blacktext">Add Person </h5>
-                <button
-                  type="button"
-                  className="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                  onClick={this.handleModalDismiss}
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body lessMargin"></div>
-              <input
-                type="text"
-                className="form-control addTaskTextArea"
-                aria-describedby="text"
-                placeholder="Insert interests here!"
-                value={this.state.noteText}
-                onChange={this.updateNoteText}
-              ></input>
-
-              <div className="form-group mx-sm-3 mb-2">
-                <input
-                  className="form-control"
-                  type="date"
-                  onChange={this.handleDateChange}
-                  value={this.state.dateSelected}
-                ></input>
-              </div>
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-dismiss="modal"
-                onClick={this.handleModalDismiss}
-              >
-                Done
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="col-1">
+      <div>
+        <div className="col-2">
           <button
             id="addButton"
-            className="btn btn-info btn btn-md "
+            className="btn add-button"
             onClick={this.handleClick}
           >
             <i className="fa fa-plus"> </i>
           </button>
         </div>
+
+        {/* ADD modal */}
+        <div className="row">
+          <div
+            className={this.state.showModal ? "modal isVisible" : "modal"}
+            id="modalEditForm"
+            tabIndex="-1"
+            role="dialog"
+            aria-labelledby="myModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-header text-center">
+                  <span className="makeItPink">
+                    add a birthday...
+                  </span>
+                  <button
+                    type="button"
+                    className="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                    onClick={this.handleModalDismiss}
+                  >
+                    <span aria-hidden="true" className="makeItPink">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body mx-3">
+                  <div className="md-form">
+                    <label
+                      data-error="wrong"
+                      data-success="right"
+                      htmlFor="form8"
+                    >
+                      Name:
+                    </label>
+                    <textarea
+                      type="text"
+                      id="form8"
+                      className="md-textarea form-control"
+                      rows="1"
+                      placeholder="Name"
+                      onChange={this.handleNewName}
+                    ></textarea>
+                  </div>
+                  <br></br>
+                  <label
+                    data-error="wrong"
+                    data-success="right"
+                    htmlFor="form8"
+                  >
+                    Birthday:
+                  </label>
+                  <div className="form-group mx-sm-3 mb-2">
+                    <input
+                      className="form-control"
+                      type="date"
+                      onChange={this.handleBirthday}
+                    ></input>
+                    <label
+                      data-error="wrong"
+                      data-success="right"
+                      htmlFor="form8"
+                    ></label>
+                  </div>
+
+                  <div className="md-form">
+                    <textarea
+                      type="text"
+                      id="form8"
+                      className="md-textarea form-control"
+                      rows="2"
+                      placeholder="Add notes here"
+                      onChange={this.updateNoteText}
+                    ></textarea>
+                    <label
+                      data-error="wrong"
+                      data-success="right"
+                      htmlFor="form8"
+                    ></label>
+                  </div>
+                </div>
+                <div className="modal-footer d-flex justify-content-center">
+                  <button
+                    className="btn btn-unique done-button"
+                    onClick={this.handleClickAdd}
+                  >
+                    Done
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
-    );
+    )
   }
+
 }
+
 export default AddPerson;
--- src/Components/BirthdayList.js
+++ src/Components/BirthdayList.js
@@ -1,29 +1,40 @@
-import React from "react";
+import React from 'react';
 import moment from "moment";
-import "font-awesome/css/font-awesome.min.css";
-import button from "react-bootstrap/Button";
+import "../App.js";
 
 class BirthdayList extends React.Component {
+
   state = {
     newBirthdayNote: "",
-    showDropdown: false
+    updatedName: "",
+    updatedDOB: "",
+    showDropdown: false,
+    showModal: false
   };
 
-  editBirthday = birthday => {
-    console.log("Hi");
-    const newBirthdays = prompt("Update birthday", this.props.text);
-    this.props.editBirthdayFunc(this.props.id, newBirthdays);
-    console.log("id is" + this.props.id);
-    console.log(newBirthdays);
-    return newBirthdays;
-  };
+  formatDateDisplay = props => {
+    if (this.props.isBirthdayToday) {
+      return (
+        <div>
+          <h2 className="birthday-today">turns {props.nextAge} today!</h2>
+          <h3>{moment(props.dateOfBirth).format("MMM Do")}</h3>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h2>{this.props.nextBirthday.fromNow()}</h2>
+          <h3>turns <span className="years">{props.nextAge}</span> on {moment(props.dateOfBirth).format("MMM Do")}</h3>
+        </div>
+      )
+    }
+  }
 
-  handleDelete = e => {
-    this.props.deleteTaskFunc(this.props.id);
+  handleClickDelete = () => {
+    this.props.deleteBirthdayFunc(this.props.id);
   };
 
   handleClickDropdown = () => {
-    console.log(this.state.showDropdown);
     if (this.state.showDropdown === false) {
       this.setState({
         showDropdown: true
@@ -35,61 +46,214 @@ class BirthdayList extends React.Component {
     }
   };
 
+  // FUNCTION FOR EDITING BIRTHDAYS:
+  handleClickEdit = () => {
+    if (this.state.showModal === false) {
+      this.setState({
+        showModal: true
+      });
+    }
+  };
+
+  // FUNCTION FOR EDITING BIRTHDAYS:
+  handleBirthday = e => {
+    this.setState({
+      updatedDOB: e.target.value
+    });
+  };
+
+  // FUNCTION FOR EDITING BIRTHDAYS:
+  updateNameText = e => {
+    this.setState({
+      updatedName: e.target.value
+    });
+  };
+
+  // FUNCTION FOR EDITING BIRTHDAYS:
+  updateNoteText = e => {
+    this.setState({
+      newBirthdayNote: e.target.value
+    });
+  };
+
+  // FUNCTION FOR EDITING BIRTHDAYS:
+  handleModalDismiss = () => {
+    this.props.editBirthdayFunc(
+      this.props.id,
+      this.state.newBirthdayNote,
+      this.state.updatedName,
+      this.state.updatedDOB
+    );
+    this.setState({
+      showModal: false
+    });
+  };
+
   render() {
     return (
-      <div className="row">
-        <div className="col-4">{this.props.name}</div>
-        <div className="col-2">
-          {moment().diff(this.props.dateOfBirth, "years")}
-        </div>
-        <div className="col-2">
-          <button type="button" className="btn btn-link">
-            {" "}
-            <a
-              href={
-                moment().diff(this.props.dateOfBirth, "years") > 12
-                  ? "https://www.amazon.com/s?k=gift&ref=nb_sb_noss_2"
-                  : "https://www.amazon.com/gcx/Gifts-for-Kids-4-7/gfhz/?canBeGiftWrapped=false&categoryId=kid4-neutral&isPrime=false&starRatingFrom=0"
-              }
-              target="blank"
-            >
-              <i className="fa fa-gift"></i>
-            </a>
-          </button>
-        </div>
-        <div className="col-2">
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={this.editBirthday}
-          >
-            edit
-          </button>
-        </div>
-        <div className="col-2">
-          <p>
-            {" "}
-            <button
-              className="btn btn-primary"
-              type="button"
-              onClick={this.handleClickDropdown}
-            >
-              <i className="fa fa-caret-down"></i>{" "}
-            </button>
-          </p>
-        </div>
+
+      <div className="container">
         <div className="row">
-          <div
-            className="collapse"
-            id="collapseExample"
-            style={{ display: this.state.showDropdown ? "inline" : "none" }}
-          >
-            <div className="card card-body blacktext">{this.props.text}</div>
+
+        {/* this is the EDIT modal */}
+        <div
+          className={
+            this.state.showModal ? "modal  isVisible blacktext" : "modal "
+          }
+          id="modalEditForm"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="myModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header text-center">
+                <h4 className="modal-title makeItPink">
+                  Edit {this.props.name}'s info
+                </h4>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                  onClick={this.handleModalDismiss}
+                >
+                  <span aria-hidden="true" className="makeItPink">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body mx-3">
+                <div className="md-form">
+                  <label
+                    data-error="wrong"
+                    data-success="right"
+                    htmlFor="form8"
+                  >
+                    Edit name:
+                  </label>
+                  <textarea
+                    type="text"
+                    id="form8"
+                    className="md-textarea form-control"
+                    rows="1"
+                    placeholder={this.props.name}
+                    onChange={this.updateNameText}
+                  ></textarea>
+                </div>
+                <br></br>
+                <label data-error="wrong" data-success="right" htmlFor="form8">
+                  Edit birthday:
+                </label>
+                <div className="form-group mx-sm-3 mb-2">
+                  <input
+                    className="form-control"
+                    type="date"
+                    onChange={this.handleBirthday}
+                  ></input>
+                  <label
+                    data-error="wrong"
+                    data-success="right"
+                    htmlFor="form8"
+                  ></label>
+                </div>
+                <div className="md-form">
+                  <label
+                    data-error="wrong"
+                    data-success="right"
+                    htmlFor="form8"
+                  >
+                    Edit interests:
+                  </label>
+                  <textarea
+                    type="text"
+                    id="form8"
+                    className="md-textarea form-control"
+                    rows="2"
+                    placeholder={this.props.text}
+                    value={this.state.noteText}
+                    onChange={this.updateNoteText}
+                  ></textarea>
+                </div>
+              </div>
+              <div className="modal-footer d-flex justify-content-center">
+                <button
+                  className="btn btn-unique done-button"
+                  onClick={this.handleModalDismiss}
+                >
+                  Done
+                </button>
+              </div>
+            </div>
           </div>
         </div>
+
+          {/* this is each BIRTHDAY */}
+          <div className="col-5">
+            <button className="btn info-dropdown"
+              onClick={this.handleClickDropdown}
+            >
+              <i className="fa fa-caret-down" />
+            </button>
+            <h1>{this.props.name}</h1>
+          </div>
+          <div className="col-4">
+            {this.formatDateDisplay(this.props)}
+          </div>
+          <div className="col-2">
+            <button className="btn edit"
+              onClick={this.handleClickEdit}
+            >EDIT</button>
+          </div>
+          <div className="col-1">
+            <button className="btn delete"
+              onClick={this.handleClickDelete}
+            >
+              <i className="fa fa-trash" />
+            </button>
+          </div>
+
+          {/* this is the BIRTHDAY dropdown */}
+          <div className="container">
+            <div className="row">
+              <div
+                className="col-12"
+                style={{ display: this.state.showDropdown ? "inline" : "none" }}
+              >
+                <span className="card interests">INTERESTS: {this.props.text}</span>
+
+                <button type="button" className="btn gift ml-2">
+                  <a
+                    href={
+                      (this.props.nextAge, "years") > 12
+                        ? "https://www.amazon.com/s?k=gift&ref=nb_sb_noss_2"
+                        : "https://www.amazon.com/gcx/Gifts-for-Kids-4-7/gfhz/?canBeGiftWrapped=false&categoryId=kid4-neutral&isPrime=false&starRatingFrom=0"
+                    }
+                    target="blank"
+                  >
+                    <i className="fa fa-gift"></i>
+                  </a>
+                </button>
+
+                <button type="button" className="btn envelope mx-2">
+                    <i className="fa fa-envelope"></i>
+                </button>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="row">
+          <div className="col-12">
+            <hr className="rule" />
+          </div>
+        </div>
+
       </div>
-    );
+    )
   }
+
 }
 
 export default BirthdayList;
