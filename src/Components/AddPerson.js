4ff35cee7551f21780325e8c5464631496aa2c0b
Marsha <marshaleonoradunn@gmail.com>
2020-01-06 09:06:35 +0000

    Revert "Merge branch 'newer-branch' into gen-branch"
    This reverts commit 1d1fa77af3e82560ddfc9d0403e958e75a7306fd, reversing
    changes made to 7df67826725b5f4819a80321d5514cf1ea8cbde3.

--- src/Components/AddPerson.js
+++ src/Components/AddPerson.js
@@ -56,16 +56,8 @@ class AddPerson extends React.Component {
     });
   };
 
-  handleNewName = e => {
-    this.setState({
-      name: e.target.value
-    });
-    console.log("WOOP", this.state.name);
-  };
-
   render() {
     return (
-
       <div>
         <div className="col-2">
           <button
@@ -77,12 +69,10 @@ class AddPerson extends React.Component {
           </button>
         </div>
 
-
         {/* ADD modal */}
         <div className="row">
           <div
             className={this.state.showModal ? "modal isVisible" : "modal"}
-
             id="modalEditForm"
             tabIndex="-1"
             role="dialog"
@@ -92,7 +82,6 @@ class AddPerson extends React.Component {
             <div className="modal-dialog" role="document">
               <div className="modal-content">
                 <div className="modal-header text-center">
-
                   <span className="makeItPink">
                     add a birthday...
                   </span>
@@ -173,6 +162,7 @@ class AddPerson extends React.Component {
             </div>
           </div>
         </div>
+
       </div>
     )
   }
