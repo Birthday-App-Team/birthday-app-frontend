a961e13242618d3f1079c6a98490116fe7185b2f
Genevieve Heald <gennaheald@outlook.com>
2020-01-11 16:26:23 +0000

    Updates

---
+++ precache-manifest.ab0e0d47efdeaafc8dac88104d363435.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9f1b82bdda887b55cc4dde0bdea5c1",
+    "url": "/birthday-app-frontend/index.html"
+  },
+  {
+    "revision": "b3fd2f4a2f270225210f",
+    "url": "/birthday-app-frontend/static/css/main.b9803bf2.chunk.css"
+  },
+  {
+    "revision": "7a94a1fd4fb5e8ebce7f",
+    "url": "/birthday-app-frontend/static/js/2.4b84f1a8.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/birthday-app-frontend/static/js/2.4b84f1a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3fd2f4a2f270225210f",
+    "url": "/birthday-app-frontend/static/js/main.0dd4c43f.chunk.js"
+  },
+  {
+    "revision": "32a42bd031bdcc8fe426",
+    "url": "/birthday-app-frontend/static/js/runtime-main.cbcffd45.js"
+  },
+  {
+    "revision": "3ffac5c095ce8de5719a6a0cc9ade699",
+    "url": "/birthday-app-frontend/static/media/birthday-party.3ffac5c0.jpg"
+  },
+  {
+    "revision": "c26f4db0ad5c77199e018f090209f6d1",
+    "url": "/birthday-app-frontend/static/media/graph-birthday.c26f4db0.jpg"
+  },
+  {
+    "revision": "81f66d3141a8a4363a3db13e09062158",
+    "url": "/birthday-app-frontend/static/media/logo.81f66d31.png"
+  }
+]);
